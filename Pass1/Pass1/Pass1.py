f4c45c91b08492c7a105e655267f1d3c09900f8a
Khertus <sebek927@gmail.com>
2018-04-30 14:02:57 +0200

    Update Pass1.py

--- Pass1/Pass1/Pass1.py
+++ Pass1/Pass1/Pass1.py
@@ -2,11 +2,14 @@
 
 def bigLetter(passw):
     """Funkcja do sprawdzania czy jest duża litera w hasle"""
-    pass
+    for x in passw:
+        if ord("x") >= 65 and ord("x") <= 90:
+            return True
 
 def specialchar(passw):
     """Funkcja do sprawdzania czy jest znak specjalny w hasle"""
-    pass
+    for x in passw:
+        pass
 
 def leng(passw):
     """Funkcja do sprawdzania czy haslo ma przynajmniej 8 znakow"""
@@ -17,3 +20,7 @@ good = None
 while good != True:
     passw = input("Give me password: ")
     passw2 = input("Re-type password please: ")
+    
+    if passw != passw2:
+        
+        
