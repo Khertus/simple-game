034582d415412ae5bf286a270055f22d5caba217
Seba Biernat <sebek927@gmail.com>
2018-05-01 21:22:51 +0200

    Prawie gotowe

--- Pass1/Pass1/Pass1.py
+++ Pass1/Pass1/Pass1.py
@@ -5,22 +5,40 @@ def bigLetter(passw):
     for x in passw:
         if ord("x") >= 65 and ord("x") <= 90:
             return True
+        return False
 
-def specialchar(passw):
+def specialChar(passw):
     """Funkcja do sprawdzania czy jest znak specjalny w hasle"""
     for x in passw:
         pass
 
 def leng(passw):
     """Funkcja do sprawdzania czy haslo ma przynajmniej 8 znakow"""
-    return len(passw >= 8)
+    return len(passw) >= 8
 
-good = None
+def samePass(passw,passw2):
+    """Funkcja dziala jesli podane hasla nie sa te same"""
+    while passw != passw2:
+        passw = input("Password incorrect, please try again: ")
+        passw2 = input("Re-type password please: ")
+    return passw,passw2
 
-while good != True:
+good = [None,None,None]
+
+while good != [True,True,True]:
     passw = input("Give me password: ")
     passw2 = input("Re-type password please: ")
-    
+
     if passw != passw2:
-        
+       passw,passw2 = samePass(passw,passw2)
+
+    if leng(passw) == True:
+        good[0] = True
+    else:
+        print("Password does not meet the requirement: Lenght")
+        continue
+
+    print(good)
+
+print("Password Good")
         
